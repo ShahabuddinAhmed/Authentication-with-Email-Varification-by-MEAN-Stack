4ea39d555c70a3c91064966b4fc5d3033a852a55
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-30 13:33:14 +0600

    account rounting module is added

--- Authentication-UI/src/app/account/account-rounting.module.ts
+++ Authentication-UI/src/app/account/account-rounting.module.ts
@@ -1,3 +1,4 @@
+import { AuthCheckService } from './../auth/auth-check.service';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,8 +6,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'register', component: RegisterComponent, canActivate: [AuthCheckService] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthCheckService] }
 ];
 
 @NgModule({
